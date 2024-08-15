ac8da640b703c6bb2b9617e99082c032fdeaaa8a
shuzoe <zoe.shu.wm@gmail.com>
2024-08-15 20:41:14 +0800

    finished leaderboard module

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GameRecord } from '../interface/game-record';
+import { RankingService } from '../services/ranking.service';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.scss']
+  styleUrls: ['./leaderboard.component.scss'],
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
+
+  @Input() limit = 100;
+  @Input() isRequestFetchData: boolean = false;
+
+  initDataList: any;
+  rankingList: GameRecord[] = [];
+
+  constructor(private rankingService: RankingService) {
+
+  }
+  ngOnInit() {
+    this.rankingList = this.rankingService.getRanking(this.limit);
+    this.rankingService.getUpdates(this.limit).subscribe(data => {
+      this.rankingList = [];
+      data.forEach((doc) => {
+        this.rankingList.push(doc);
+      });
+    });
+  }
+
+  refreshBoard(){
+    this.rankingList = this.rankingService.getRanking(this.limit);
+  }
+
+  ngOnChanges(){
+    if(this.isRequestFetchData){
+      this.refreshBoard();
+    }
+  }
 
 }
--- src/app/leaderboard/leaderboard.module.ts
+++ src/app/leaderboard/leaderboard.module.ts
@@ -4,6 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LeaderboardRoutingModule } from './leaderboard-routing.module';
 import { LeaderboardComponent } from './leaderboard.component';
 
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from 'src/environment';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +18,18 @@ import { LeaderboardComponent } from './leaderboard.component';
   ],
   imports: [
     CommonModule,
-    LeaderboardRoutingModule
+    LeaderboardRoutingModule,
+    provideFirebaseApp(
+      () => initializeApp(environment.firebase)), 
+      provideAuth(() => getAuth()), 
+      provideFirestore(() => getFirestore(),
+    ),
+  ],
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+  ],
+  exports: [
+    LeaderboardComponent
   ]
 })
 export class LeaderboardModule { }
