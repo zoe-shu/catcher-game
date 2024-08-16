5c2a50313612d70c880ee4121456158611626387
shuzoe <zoe.shu.wm@gmail.com>
2024-08-16 18:31:08 +0800

    add script & style for mobile 100vh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zoe-catcher-app';
+  documentHeight: any;
+  
+  ngOnInit(){
+    this.documentHeight = () => {
+      const doc = document.documentElement
+      doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener('resize', this.documentHeight)
+    this.documentHeight()
+  }
+  
 }
